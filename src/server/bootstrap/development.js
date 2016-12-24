df3160ac36d250ab80b3828036f3a7eb0fb013a1
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-12-24 13:15:29 +0100

    [Review] MVP of the build page

---
+++ src/server/bootstrap/development.js
@@ -0,0 +1,12 @@
+// @flow weak
+
+/**
+ * This file is needed in order to make the code work on the node environement
+ * without webpack.
+ */
+
+// Set the /src folder as root
+const path = require('path');
+require('app-module-path').addPath(path.join(__dirname, '../../'));
+
+require('../app');
